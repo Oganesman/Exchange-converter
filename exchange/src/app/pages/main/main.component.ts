cd26db65c54a99df51bf7ad9942fbfe274b38a36
Oganesman <96392987+qweRTyiOp123789@users.noreply.github.com>
2022-10-05 23:20:46 +0300

    improve code

--- exchange/src/app/pages/main/main.component.ts
+++ exchange/src/app/pages/main/main.component.ts
@@ -15,21 +15,24 @@ export class MainComponent {
 		ccy: String,
 		sale: String
 	}>
+
 	//select
 	public selects: Array<{ currency: String }> = [
 		{ currency: 'UAH' },
 		{ currency: 'USD' },
 		{ currency: 'EUR' },
 	]
+
 	constructor(public ms: MainService) {
 		this.ms.getExchanges()
 			.subscribe((data: any) => {
 				this.exchanges = data.splice(0, 2)
+				this.exchanges.unshift({base_ccy:'UAH',buy:'1',ccy:'UAH',sale:'1'})
 				this.exchanges.map(el => {
 					el.buy = el.buy.slice(0, 5)
 					el.sale = el.sale.slice(0, 5)
 				})
-				if(this.ms.buy)
+				if(this.ms.buyOrSale)
 				this.ms.convertBuy(this.exchanges)
 				else
 				this.ms.convertSale(this.exchanges)
